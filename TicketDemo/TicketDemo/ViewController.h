899dfc39c7d192462a8f3ff89d2697f1cd97376f
Matt Jankowiak <mjankowiak@solstice-consulting.com>
2015-06-18 20:08:37 -0500

    initial commit

---
+++ TicketDemo/TicketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicketDemo
+//
+//  Created by Matt Jankowiak on 6/18/15.
+//  Copyright (c) 2015 Solstice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TicketDemo/TicketDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicketDemo
+//
+//  Created by Matt Jankowiak on 6/18/15.
+//  Copyright (c) 2015 Solstice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
